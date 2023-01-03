605b0421713979cc663c144b1c4e652836785f06
ADEWUMIADENIKE <111264388+ADEWUMIADENIKE@users.noreply.github.com>
2023-01-03 13:54:08 -0800

    Create 101-locked_class.py

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 101-locked_class.py
+# Adenike
+"""Defines a locked class."""
+
+
+class LockedClass:
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
+
+    __slots__ = ["first_name"]
